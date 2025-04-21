150b648578b02b91d1cbd637667d2f6cdfa2ae62
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-04-21 20:54:02 -0300

    ajustes5

--- server.js
+++ server.js
@@ -1,51 +1,69 @@
 import express from 'express'
-import cors from 'cors'
-import path from 'path'
-import { fileURLToPath } from 'url'
 import pkg from './generated/prisma/index.js'
-
+import cors from 'cors'
 const { PrismaClient } = pkg
+const PORT = process.env.PORT || 3000;
+
 const prisma = new PrismaClient()
+
 const app = express()
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 
-// === Rotas da API ===
 app.post('/usuarios', async (req, res) => {
-  const user = await prisma.user.create({ data: req.body })
-  res.status(201).json(user)
+    const user = await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    res.status(201).json(user)
 })
 
 app.get('/usuarios', async (req, res) => {
-  const users = await prisma.user.findMany({
-    where: req.query || undefined
-  })
-  res.status(200).json(users)
+    let users = []
+
+    if(req.query){
+        users = await prisma.user.findMany ({
+            where: {
+                name: req.query.name,
+                email: req.query.email,
+                age: req.query.age
+            }
+        })
+    }else {
+
+        users = await prisma.user.findMany()
+    }
+
+    res.status(200).json(users)
 })
 
 app.put('/usuarios/:id', async (req, res) => {
-  await prisma.user.update({
-    where: { id: req.params.id },
-    data: req.body
-  })
-  res.status(201).json(req.body)
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        }, 
+
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    res.status(201).json(req.body)
 })
 
 app.delete('/usuarios/:id', async (req, res) => {
-  await prisma.user.delete({ where: { id: req.params.id } })
-  res.status(200).json({ message: "Usuário deletado com sucesso!!!" })
+    
+    await prisma.user.delete({     
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(200).json({ message: "Usuário deletado com sucesso!!!"})
 })
 
-// === Servir React (build) ===
-app.use(express.static(path.join(__dirname, 'build')))
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
-
-// === Exportar o app (sem .listen()) ===
-export default app
+app.listen(PORT, () => {})
